267a0754d57c58b0ec5cd1e0a859e1388a192942
Miguel Angel Valdez Bello <51422943+mivgubel@users.noreply.github.com>
2022-07-02 22:21:52 -0500

    Update queryProposals.ts

--- scripts/queryProposals.ts
+++ scripts/queryProposals.ts
@@ -5,7 +5,7 @@ import { CustomBallot } from "../typechain";
 
 const EXPOSED_KEY =
   "8da4ef21b864d2cc526dbdb2a120bd2874c36c9d0a1fb7f8c63d7f7a8b41de8f";
-async function main() {
+export async function main() {
   const provider = ethers.providers.getDefaultProvider("ropsten");
   const signer = new ethers.Wallet(
     process.env.PRIVATE_KEY ?? EXPOSED_KEY
